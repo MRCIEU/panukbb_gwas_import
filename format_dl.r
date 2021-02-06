44f7a16f8b456a325d3f8bba700974c46b8f3d20
Gibran Hemani <explodecomputer@gmail.com>
2021-02-06 22:52:38 +0000

    fixed issues with format

--- format_dl.r
+++ format_dl.r
@@ -1,24 +1,10 @@
 library(tidyverse)
 library(glue)
 
-args <- commandArgs(T)
-
-id <- args[1]
-datadir <- args[2]
-
-load("dl.rdata")
-
-dat <- subset(uniquelist, newid == id)
-pops <- str_split(dat$pop, ",")[[1]]
-
-outfile <- file.path(datadir, paste0(dat$newid, "_", pop, ".gz"))
-
-outfile <- paste0(dat$newid, "_", pop, ".gz")
-
-format_file <- function(infile, pop, outfile, cols)
+format_file <- function(infile, pop, outfile)
 {
 	con <- gzfile(infile, "r")
-	a <- readLines(con, n=1) %>% str_split(., "\t")
+	cols <- readLines(con, n=1) %>% str_split(., "\t") %>% {.[[1]]}
 	close(con)
 
 	low_confidence <- which(cols == paste0("low_confidence_", pop))
@@ -31,7 +17,7 @@ format_file <- function(infile, pop, outfile, cols)
 	ref <- which(cols == paste0("ref"))
 	alt <- which(cols == paste0("alt"))
 
-	if(any(grepl("af_controls", a)))
+	if(any(grepl("af_controls", cols)))
 	{
 		af <- which(cols == paste0("af_controls_", pop))
 	} else {
@@ -48,4 +34,28 @@ format_file <- function(infile, pop, outfile, cols)
 	system(glue("cat {tempoutfile} | wc -l > {nsnpfile}"))
 	system(glue("gzip {tempoutfile}"))
 }
+
+args <- commandArgs(T)
+
+id <- args[1]
+datadir <- args[2]
+
+load("dl.rdata")
+
+dat <- subset(uniquelist, newid == id)
+pops <- str_split(dat$pop, ",")[[1]]
+
+print(str(dat))
+message(paste(pops, collapse=", "))
+
+lapply(pops, function(pop)
+{
+	message(pop)
+	outfile <- file.path(datadir, "ready", paste0(dat$newid, "_", pop, ".gz"))
+	format_file(
+		infile=file.path(datadir, "raw", paste0(dat$newid, ".bgz")),
+		pop=pop,
+		outfile
+	)
+})
 
